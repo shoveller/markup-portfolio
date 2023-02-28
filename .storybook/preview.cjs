19bc4860b750df869e5f5fdc7e344530c75ee8f8
cinos81 <cinos81@gmail.com>
2023-02-28 20:20:07 +0900

    feat: add storybook html plugin

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,9 +1,14 @@
+import prettier from '../.prettierrc.json'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  html: {
+    prettier,
   },
 }
